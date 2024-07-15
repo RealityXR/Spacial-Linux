05464bbae8ba707706d4e0a6f06d4b7bca7431c4
jackedak <jackedak@gmail.com>
2024-07-15 16:13:33 -0400

    start SH file

---
+++ create_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+escape_char=$(printf "\u1b")
+versions=("24.04  " "23.04  " "22.04.4" "21.04  " "20.04.6" "19.04  " "18.04  " "17.04  " "16.04.7" "14.04.6")
+hasspace=(true true false true false true true true false true false)
+read -p "Path to ISO for base OS(skip to download): " iso;
+
+if [[ -n $iso ]] ; then
+    if [[ -e $iso ]] ; then
+        echo "File found!";
+    else
+        echo -e "\033[0;31mSpecified file not found!\033[0m";
+        exit 1;
+    fi
+else
+    echo -en "Select Ubuntu version:(arrow keys) :\e[7m\e[5m";
+    echo -n "24.04"
+    version=0
+    while :;do
+        read -rsn1 mode # get 1 character
+        if [[ $mode == $escape_char ]]; then
+            read -rsn2 mode # read 2 more chars
+        fi
+        case $mode in
+            '[A') let version++ ;;
+            '[B') let version-- ;;
+            '') break;;
+        esac
+        version=$(($version>0 ? $version : 0))
+        version=$(($version<9?$version:9))
+        echo -en "\b\b\b\b\b\b\b"
+        echo -n "${versions[version]}"
+    done
+    echo -e "\e[0m";
+    mkdir "downloads" > /dev/null 2>&1
+    versionnum=${versions[version]}
+    url="https://releases.ubuntu.com/${versionnum}/ubuntu-${versionnum}-live-server-amd64.iso"
+    filepath="downloads/${versionnum}.iso"
+    url=${url// }
+    filepath=${filepath// }
+    echo "$filepath,$url"
+    curl -L -o $filepath  $url
+fi
